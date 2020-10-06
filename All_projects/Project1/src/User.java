1ff47364b2251d36b3e9a112bd9d9273007a4fab
Sravya <sravya199968@gmail.com>
2020-10-06 09:24:03 +0530

    FC

---
+++ All_projects/Project1/src/User.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author SRAVYA
+ *
+ */
+public class User {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello all.......");
+	}
+
+}
